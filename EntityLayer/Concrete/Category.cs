27f684618d06603f920bf9a6f3d4f77d036e565b
Telman Abidov <telmanabidov00@gmail.com>
2026-02-08 17:59:38 +0400

    OOP_Entity_Layer

---
+++ EntityLayer/Concrete/Category.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string? CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+        public List<Product>? Products { get; set; }
+    }
+}
